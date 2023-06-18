7035401fe2bef82054c83466b5765a0dca35b7ad
Gibsondignity <64210963+Gibsondignity@users.noreply.github.com>
2023-06-18 22:20:21 +0000

    event list

--- tickets/models.py
+++ tickets/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from accounts.models import Users
+from django.core.exceptions import ValidationError
 
 # Create your models here.
-
+class FileField(models.FileField):
+    def __init__(self, *args, **kwargs):
+        self.max_upload_size = kwargs.pop("max_upload_size", 1024*1024)
+        super().__init__(*args, **kwargs)
+        
+    def upload_to(self, instance, filename):
+        file = instance.file
+        if file.size > self.max_upload_size:
+            raise ValidationError("File size must be less than 1 MB")
+        
 
 class Category(models.Model):
     category_name = models.CharField(max_length=65)
     price = models.DecimalField(max_digits=12, decimal_places=2)
-    user = models.ForeignKey(Users, on_delete=models.CASCADE, blank=True)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, blank=True, null=True)
     date_created = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now_add=True)
     
@@ -18,10 +28,13 @@ class Category(models.Model):
     
 class Event(models.Model):
     event_name = models.CharField(max_length=256, )
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True) 
     event_date = models.DateField()
     event_time = models.TimeField(null=True)
+    description = models.CharField(max_length=1000, blank=True, null=True)
+    picture = FileField(upload_to='gallery', blank=True, null=True)
+    location = models.CharField(max_length=65, null=True)
     date_created = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now_add=True)
     
--- tickets/views.py
+++ tickets/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
@@ -7,9 +8,9 @@ def home(request):
 
 
 
-
-
-
 def events(request):
     
-    return render(request, 'tickets/events/events.html')
+    events = Event.objects.all()
+    media_url = "http://127.0.0.1:8000/media/"
+    context = {"events": events, "media_url":media_url}
+    return render(request, 'tickets/events/events.html', context)
