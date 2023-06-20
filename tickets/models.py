cd8dc3c04759a3ccf1e9559b21e690e528ee8a4e
Gibsondignity <64210963+Gibsondignity@users.noreply.github.com>
2023-06-20 14:40:05 +0000

    changed referene to event

--- tickets/models.py
+++ tickets/models.py
@@ -14,27 +14,15 @@ class FileField(models.FileField):
             raise ValidationError("File size must be less than 1 MB")
         
 
-class Category(models.Model):
-    category_name = models.CharField(max_length=65)
-    price = models.DecimalField(max_digits=12, decimal_places=2)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    date_created = models.DateTimeField(auto_now=True)
-    date_updated = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self) -> str:
-        return f"{self.category_name} - {self.price}"
-    
-    
-    
+
 class Event(models.Model):
     event_name = models.CharField(max_length=256, )
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True) 
     event_date = models.DateField()
     event_time = models.TimeField(null=True)
     description = models.CharField(max_length=1000, blank=True, null=True)
     picture = FileField(upload_to='gallery', blank=True, null=True)
-    location = models.CharField(max_length=65, null=True)
+    location = models.CharField(max_length=65, null=True, blank=True)
     slug = models.CharField(max_length=256, blank=True, null=True)
     date_created = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now_add=True)
@@ -54,6 +42,23 @@ class Event(models.Model):
     
     def __str__(self):
         return self.event_name
+    
+    
+    
+class Category(models.Model):
+    category_name = models.CharField(max_length=65)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, blank=True, null=True)
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f"{self.category_name} - {self.price}"
+    
+    
+    
+
 
 
     
