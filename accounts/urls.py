ab78e7782a06ef4a96269bf73624c7e761b095e3
Gibsondignity <64210963+Gibsondignity@users.noreply.github.com>
2023-07-13 17:32:36 +0000

    dashboard started

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('login', views.account_login, name='login'),
+    path('account', views.account_login, name='login'),
     path('register', views.account_register, name='register'),
 ]
